c9c15172944f10081a4c74f98b6976bb4a010609
marcoesposito1996 <marcoespositoh@gmail.com>
2022-01-14 01:39:55 +0100

    ...

--- habit-tracker-web/src/components/HabitForm.js
+++ habit-tracker-web/src/components/HabitForm.js
@@ -91,7 +91,6 @@ export default function HabitForm({ uid, token }) {
                             <option value="2">2</option>
                             <option value="3">3</option>
                             <option value="4">4</option>
-                            <option value="5">5</option>
                             <option value="6">6</option>
                         </select>
                     </div>
