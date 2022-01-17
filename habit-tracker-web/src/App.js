6b9fb46a8c02baa0972f68dac20e92703da6884a
fabiaiz <fabiaiz10@gmail.com>
2022-01-17 08:48:31 +0100

    styled app.js

--- habit-tracker-web/src/App.js
+++ habit-tracker-web/src/App.js
@@ -1,37 +1,15 @@
-
-
 import './App.css';
-import Login from './components/Login';
 import RouterNav from './RouterNav';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { auth } from './firebase';
-import {
-  BrowserRouter,
-  Routes,
-  Route, Switch
-} from "react-router-dom";
-import store, {persistor} from './store';
-import {PersistGate} from 'redux-persist/integration/react';
-import {Provider} from "react-redux"
+import store, { persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from "react-redux"
 
+function App() {
 
-
-
-function App() { 
-
-  const [user, setUser] = useState(null);
-  
-  // useEffect(() => {
-  //   auth().onAuthStateChanged(user => {
-  //     setUser(user);
-  //   })
-  // }, [])
-
-  return (   
+  return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <RouterNav/>
+        <RouterNav />
       </PersistGate>
     </Provider>
   );
